533bfa7d83ebe5cc7459de51af5d51ed11daecee
Aiken Tine Ahac <ahac.aiken@gmail.com>
2022-10-02 09:57:21 +0200

    Get swagger details from .env

--- src/main.ts
+++ src/main.ts
@@ -6,19 +6,22 @@ import { config } from 'aws-sdk';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
 
   app.enableCors();
 
   const swaggerConfig = new DocumentBuilder()
     .setTitle('Socialko')
     .setContact(
-      'Aiken Tine Ahac',
-      'https://aikenahac.com',
-      'ahac.aiken@gmail.com',
+      configService.get('SWAGGER_CONTACT_NAME') ?? 'Aiken Tine Ahac',
+      configService.get('SWAGGER_CONTACT_WEBSITE') ?? 'https://aikenahac.com',
+      configService.get('SWAGGER_CONTACT_EMAIL') ?? 'ahac.aiken@gmail.com',
     )
-    .setDescription('The Socialko API description')
-    .setVersion('0.0.1')
-    .addTag('socialko')
+    .setDescription(
+      configService.get('SWAGGER_DESCRIPTION') ?? 'Socialko API Documentation',
+    )
+    .setVersion(configService.get('SWAGGER_API_VERSION') ?? '0.0.1')
+    .addTag(configService.get('SWAGGER_TAG') ?? 'socialko')
     .build();
 
   const document = SwaggerModule.createDocument(app, swaggerConfig);
@@ -26,7 +29,6 @@ async function bootstrap() {
     customSiteTitle: 'Socialko API',
   });
 
-  const configService = app.get(ConfigService);
   config.update({
     accessKeyId: configService.get('S3_ACCESS_KEY'),
     secretAccessKey: configService.get('S3_SECRET_KEY'),
