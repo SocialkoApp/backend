13900f6e498267030e83d0838e1fcba0e50431fb
Aiken Tine Ahac <ahac.aiken@gmail.com>
2023-03-14 09:32:39 +0100

    Remove unnecessary logs

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -173,7 +173,6 @@ export class UserService {
   }
 
   async check(user: Prisma.UserWhereUniqueInput) {
-    this.logger.verbose(user);
     if (!(await this.prisma.user.findUnique({ where: user }))) {
       throw new NotFoundException('This user does not exist');
     }
